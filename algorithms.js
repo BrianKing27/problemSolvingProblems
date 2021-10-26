faa7405efeb043554d6f34b5e7f56441dd1887ed
unknown <briankingDCC@gmail.com>
2021-10-26 12:55:41 -0400

    Wrote function to go through each number and check if it's divisble by 3 until you reach the final number. If it is, it prints fizz to the console instead of the number

--- algorithms.js
+++ algorithms.js
@@ -8,14 +8,24 @@
 //   3. increase by increments of 1
 //   4. Cap the loop at the number place in the parameter variable.
 
+
+// b. If a number is divisible by 3, print ‘fizz’ instead of the number
+//   1. Write a function that checks each number logged.
+//   2. Go through each number and check until you reach the final number.
+//   3. If number checked is divisible by 3 print a string to the console in it's place.
+
+
 function fizzbuzz(number){
     for(let i = 0; i <= number; i++){
         console.log(i);
+        if(i % 3 === 0){
+            console.log('fizz');
+        }
     }
+
 }
 
 fizzbuzz(100);
-// b. If a number is divisible by 3, print ‘fizz’ instead of the number
 
 // c. If a number is divisible by 5, print ‘buzz’ instead of the number
 
